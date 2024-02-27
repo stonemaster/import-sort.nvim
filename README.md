2d1cb4ef210c2db1da3d9b8f433c89e9f96629d2
André Stein <andre.stein.1985@gmail.com>
2024-02-27 12:07:18 +0100

    Initial plugin commit including README.

---
+++ README.md
@@ -0,0 +1,42 @@
+# import-sort.nvim
+
+## Purpose
+
+This [Neovim](https://neovim.io/) plugin provides a basic, programming language
+agnostic way to sort import statements through the user-defined command
+`:SortImports`.
+
+The plugin is **entirely** written in Lua, so no other dependencies are
+required.
+
+## Requirements
+
+This plugin should work with any *Neovim* version.
+
+## Installation
+
+Using [lazy.nvim](https://github.com/folke/lazy.nvim) in LUA:
+
+```lua
+{
+  -- amongst your other plugins
+  { 'stonemaster/import-sort.nvim' }
+}
+```
+## Usage
+
+Type `:SortImports` and the current buffer is magically replaced with your
+imports properly sorted.
+
+## Supported Languages & Logic
+
+The logic is extremely simple: the fallback import statement is to look for
+`import` statements at the beginning of the code file. This is the common
+import statement for a lot of languages. There is a special case for C/C++
+buffer where `#include` is guessed. Other special cases can easily be added
+(open either an issue or a PR).
+
+Import statements that are divided into blocks by blank lines are sorted
+individually.
+
+Sorting of imports is only supported at the beginning of a file.
---
+++ lua/import-sort.lua
@@ -0,0 +1,86 @@
+-- Function to sort lines based on import statement
+--
+-- @param lines an array of lines
+-- @param import_statement the name of the import statement that is used to
+--   detect code lines using the import statement.
+-- @return the import sorted version of @c lines
+local function sort_lines(lines, import_statement)
+    -- enumeration for states
+    local BEFORE = 0
+    local IMPORTS = 1
+    local AFTER = 2
+
+    local mode = BEFORE
+    local before_lines = {}
+    local import_blocks = { {} }
+    local after_lines = {}
+
+    local function array_for()
+        if mode == BEFORE then
+            return before_lines
+        elseif mode == IMPORTS then
+            return import_blocks[#import_blocks]
+        elseif mode == AFTER then
+            return after_lines
+        end
+    end
+
+    for _, line in ipairs(lines) do
+        if line:find(import_statement, 1, true) == 1 then
+            if mode == AFTER then
+                table.insert(after_lines, line)
+            else
+                mode = IMPORTS
+                table.insert(array_for(), line)
+            end
+        elseif line:match("^%s*$") then
+            if mode == IMPORTS then
+                table.insert(import_blocks, {})
+            end
+            table.insert(array_for(), line)
+        else
+            if mode == IMPORTS then
+                mode = AFTER
+            end
+            table.insert(array_for(), line)
+        end
+    end
+
+    local final_contents = before_lines
+    for _, import_block in ipairs(import_blocks) do
+        table.sort(import_block)
+        vim.list_extend(final_contents, import_block)
+    end
+    vim.list_extend(final_contents, after_lines)
+
+    return final_contents
+end
+
+-- Function to guess import statement based on file extension
+local function guess_import_statement(language)
+    if language == "c" or language == "cpp" then
+        return "#include"
+    else
+        return "import"
+    end
+end
+
+
+-- The actual main function attached to :SortImports
+local function sort_imports_in_current_buffer()
+    local buffer_contents = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
+    local language = vim.bo.filetype
+    local import_statement = guess_import_statement(language)
+
+    local final = sort_lines(buffer_contents, import_statement)
+    vim.api.nvim_buf_set_lines(0, 0, vim.api.nvim_buf_line_count(0), true, final)
+end
+
+-- Setup plugin
+local function setup()
+    vim.api.nvim_create_user_command("SortImports", sort_imports_in_current_buffer, {})
+end
+
+setup()
+
+return {}
